a53f79b885d17287cf089e357357129d7b26303d
stserhio <2411sergo@gmail.com>
2023-04-10 20:33:22 +0300

    lessons11-12

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+
+const Header = () =>{
+    return(
+        <header className='header'>
+        <img src='https://www.logodesign.net/logo/letter-d-inside-polygon-4839ld.png'></img>
+      </header>
+    )
+}
+
+export default Header
---
+++ src/components/Profile.jsx
@@ -0,0 +1,14 @@
+
+
+const Profile = () => {
+    return(
+        <div className='content'>
+        <div>
+          {/* <img src='https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8&w=1000&q=80'></img> */}
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/800px-Image_created_with_a_mobile_phone.png"></img>
+      </div>
+      </div> 
+    )
+}
+
+export default Profile
